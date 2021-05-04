507e4fcd0ef22d38cad8970d0fbfc22882fc5fb3
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2021-05-05 00:53:50 +0900

    Update mac.js

--- mac.js
+++ mac.js
@@ -1,4 +1,10 @@
-function p1(){
- document.getElementById("au1").load();
- document.getElementById("au1").play();
-};
+function divv(){
+var media = document.getElementById("au1");
+media.volume = 0.8;
+media.load();
+media.play();
+}
+
+function divw(){
+document.getElementById("au1").pause();
+}
