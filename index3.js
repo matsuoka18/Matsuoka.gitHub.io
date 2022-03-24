4451d156eae0a0c2b7f610206b4cc6fc2b5cc7d4
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2022-03-24 18:39:54 +0900

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,51 @@
+function start1(){
+  var height = window.screen.height;
+  var width = window.screen.width;
+   function start(){
+if(height == 667 && width == 375){
+  var d = document;
+		var link = d.createElement('link');
+		link.href = 'style2.1.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		var h = d.getElementsByTagName('head')[0];
+		h.appendChild(link);
+}
+else if(height == 812 && width == 375){
+  var d = document;
+		var link = d.createElement('link');
+		link.href = 'style2.2.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		var h = d.getElementsByTagName('head')[0];
+		h.appendChild(link);
+}
+else if(height == 896 && width == 414){
+  var d = document;
+		var link = d.createElement('link');
+		link.href = 'style2.3.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		var h = d.getElementsByTagName('head')[0];
+		h.appendChild(link);
+}
+else if(height == 844 && width == 390){
+  var d = document;
+		var link = d.createElement('link');
+		link.href = 'style2.4.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		var h = d.getElementsByTagName('head')[0];
+		h.appendChild(link);
+}
+else if(height == 926 && width == 428){
+  var d = document;
+		var link = d.createElement('link');
+		link.href = 'style2.5.css';
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		var h = d.getElementsByTagName('head')[0];
+		h.appendChild(link);
+}
+}
+}
